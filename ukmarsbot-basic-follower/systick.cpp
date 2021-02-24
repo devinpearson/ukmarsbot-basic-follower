3c1045f2e19b7c343b0856558f88544fd927dc02
Peter Harrison <peter.harrison@helicron.net>
2021-02-24 15:21:43 +0000

    ensure battery comentation scle is set in systick

--- ukmarsbot-basic-follower/systick.cpp
+++ ukmarsbot-basic-follower/systick.cpp
@@ -1,9 +1,10 @@
 #include "systick.h"
-#include <arduino.h>
 #include "encoders.h"
 #include "motion.h"
 #include "profile.h"
 #include "sensors.h"
+#include "motors.h"
+#include <arduino.h>
 
 volatile uint32_t ticks = 0;
 void setupSystick(int frequency) {
@@ -14,14 +15,14 @@ void setupSystick(int frequency) {
   bitSet(TCCR2B, CS22);
   bitClear(TCCR2B, CS21);
   bitSet(TCCR2B, CS20);
-  OCR2A = 249;  // (16000000/128/500)-1 => 500Hz
-  // OCR2A = 124;    // (16000000/128/1000)-1 => 1000 Hz
+  OCR2A = 249; // (16000000/128/500)-1 => 500Hz
   bitSet(TIMSK2, OCIE2A);
 }
 
 ISR(TIMER2_COMPA_vect) {
   ticks++;
   sensorsUpdate();
+  set_motor_battery_comp(batteryVolts);
   encoderUpdate();
   steeringUpdate();
   rot.adjust(gSteeringControl);
